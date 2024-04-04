65ae22f648bd211de1c5e1fccc1bdd996be1595d
kydyrmyshov107 <kydyrmysovnursultan@gmail.com>
2024-04-04 22:24:52 +0600

    update dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import Layout from './components/layout/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pagesSide/components/layout/Layout';
+import Login from './pagesAuth/components/pages/login/Login';
+import Register from './pagesAuth/components/pages/register/Register';
 
 const App = () => {
 	return (
 		<>
-			<Layout />
+			<Routes>
+				<Route path="/*" element={<Layout />} />
+				<Route path="/auth/login" element={<Login />} />
+				<Route path="/auth/register" element={<Register />} />
+			</Routes>
 		</>
 	);
 };
---
+++ src/pagesAuth/components/layout/Layout.tsx
@@ -0,0 +1,5 @@
+const Layout = () => {
+	return <div></div>;
+};
+
+export default Layout;
