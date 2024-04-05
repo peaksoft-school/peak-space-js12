0c792b997280ce5c61f12d30dda5a1693f0df70f
kydyrmyshov107 <kydyrmysovnursultan@gmail.com>
2024-04-05 14:47:56 +0600

    added userImg

--- src/pagesSide/components/layout/LayoutSide.tsx
+++ src/pagesSide/components/layout/LayoutSide.tsx
@@ -1,20 +1,12 @@
-import AddStory from '@/src/UI/addStory/AddStory';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import scss from './LayoutSide.module.scss';
-import AvatarStory from '@/src/UI/avatarStory/AvatarStory';
-import ProfilUser from '@/src/UI/profilUser/ProfilUser';
 
 const LayoutSide = () => {
 	return (
 		<div className={scss.Layout}>
 			<Header />
-			<main>
-				main
-				<AddStory />
-				<AvatarStory />
-				<ProfilUser />
-			</main>
+			<main>main</main>
 			<Footer />
 		</div>
 	);
