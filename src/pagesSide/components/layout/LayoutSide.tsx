f085218d438ff9cbb0a265aec320c0ae69be6ab1
kydyrmyshov107 <kydyrmysovnursultan@gmail.com>
2024-04-06 20:37:39 +0600

    update edit-user-img

--- src/pagesSide/components/layout/LayoutSide.tsx
+++ src/pagesSide/components/layout/LayoutSide.tsx
@@ -1,16 +1,12 @@
 import scss from './LayoutSide.module.scss';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import UserEditProfile from '@/src/UI/userImages/UserEditProfile';
 
 const LayoutSide = () => {
 	return (
 		<div className={scss.Layout}>
 			<Header />
-			<main>
-				main
-				<UserEditProfile />
-			</main>
+			<main>main</main>
 			<Footer />
 		</div>
 	);
