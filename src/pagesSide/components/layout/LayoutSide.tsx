4d71f452fbc508b4a4cd35ba8b42e2291907e003
mac <mac@macs-MacBook-Pro.local>
2024-04-08 01:11:38 -0700

    added select

--- src/pagesSide/components/layout/LayoutSide.tsx
+++ src/pagesSide/components/layout/LayoutSide.tsx
@@ -1,15 +1,12 @@
 import scss from './LayoutSide.module.scss';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import CustomSelect from '@/src/UI/customSelect/CustomSelect';
 
 const LayoutSide = () => {
 	return (
 		<div className={scss.Layout}>
 			<Header />
-			<main>
-				<CustomSelect />
-			</main>
+			<main></main>
 			<Footer />
 		</div>
 	);
