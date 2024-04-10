9af187bc7b753f99500ed47facd62e045877c2e9
kydyrmyshov107 <kydyrmysovnursultan@gmail.com>
2024-04-10 15:06:36 +0600

    fixed page-auth

--- src/pagesAuth/components/pages/newPassword/NewPassword.tsx
+++ src/pagesAuth/components/pages/newPassword/NewPassword.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import scss from './NewPassword.module.scss';
 import peakSpace from '../../../../assets/peakSpace.png';
 import { Input } from 'antd';
@@ -5,24 +6,38 @@ import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CustomButtonBold from '@/src/UI/customButton/CustomButtonBold';
+import { Controller, useForm } from 'react-hook-form';
+
+interface ErrorProps {
+	password: string;
+}
 
 const NewPassword = () => {
-	const [password, setPassword] = useState<string>('');
 	const [showPassword] = useState<boolean>(false);
 	const navigate = useNavigate();
 
-	const handlePasswordChange = (e: { target: { value: string } }) => {
-		setPassword(e.target.value);
-	};
 	const navigateUserPassword = () => {
 		navigate('/auth/login');
+	};
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+		reset
+	} = useForm<ErrorProps>({ mode: 'onBlur' });
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const onSubmit = (data: any) => {
+		console.log(data);
+
+		reset();
 	};
 
 	return (
 		<div className={scss.NewPassword}>
 			<div className={scss.bar}>
 				<div className="container">
-					<div className={scss.content}>
+					<form onSubmit={handleSubmit(onSubmit)} className={scss.content}>
 						<img src={peakSpace} alt="" />
 						<div className={scss.section}>
 							<p className={scss.title}>Не удается войти?</p>
@@ -30,21 +45,41 @@ const NewPassword = () => {
 								Введите свой электронный адрес, имя пользователя или номер
 								телефона
 							</p>
-							<Input.Password
-								value={password}
-								onChange={handlePasswordChange}
-								iconRender={(visible) =>
-									visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
-								}
-								placeholder="Пароль"
-								onPressEnter={() => console.log('Нажат Enter')}
-								className={scss.inputPassword}
-								visibilityToggle
-								type={showPassword ? 'text' : 'password'}
+							<Controller
+								name="password"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Пароль обязателен к заполнению' }}
+								render={({ field }) => (
+									<Input.Password
+										{...field}
+										iconRender={(visible) =>
+											visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+										}
+										placeholder=" Пароль"
+										className={scss.inputPassword}
+										visibilityToggle
+										type={showPassword ? 'text' : 'password'}
+										style={{
+											borderColor: errors.password ? 'red' : '',
+											backgroundColor: errors.password
+												? 'rgba(255, 0, 0, 0.122)'
+												: '',
+											outline: 'none'
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+									/>
+								)}
 							/>
+							{errors.password && (
+								<p className={scss.errorPassword}>{errors.password.message}</p>
+							)}
 						</div>
-						<CustomButtonBold children="Войти" onClick={navigateUserPassword} />
-					</div>
+						<CustomButtonBold children="Войти" type="submit" />
+					</form>
 				</div>
 			</div>
 		</div>
--- src/pagesAuth/components/pages/registration/Registration.tsx
+++ src/pagesAuth/components/pages/registration/Registration.tsx
@@ -1,27 +1,28 @@
-import { useState } from 'react';
-import CustomInput from '@/src/UI/customInput/CustomInput';
-import peakSpaceImg from '../../../../assets/peakSpace.png';
-import scss from './Registration.module.scss';
 import { Input, Checkbox } from 'antd';
 import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
+import { Controller, useForm } from 'react-hook-form';
+
 import CustomButtonBold from '@/src/UI/customButton/CustomButtonBold';
+import peakSpaceImg from '../../../../assets/peakSpace.png';
+import scss from './Registration.module.scss';
+import { ChangeEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+interface ErrorObject {
+	message: string;
+	password: string;
+	email: string;
+	Фамилия: string;
+	Имя: string;
+	nameUser: string;
+}
 
 const Registration = () => {
-	const [inputValue, setInputValue] = useState('');
-	const [password, setPassword] = useState<string>('');
-	const [confirmPassword, setConfirmPassword] = useState<string>('');
-	const [showPassword, setShowPassword] = useState<boolean>(false);
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
+	const [password, setPassword] = useState('');
 
-	const handleInputChange = (e: { target: { value: string } }) => {
-		setInputValue(e.target.value);
-	};
-
-	const handlePasswordChange = (e: { target: { value: string } }) => {
-		setPassword(e.target.value);
-	};
-
-	const handleConfirmPasswordChange = (e: { target: { value: string } }) => {
+	const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setConfirmPassword(e.target.value);
 	};
 
@@ -29,54 +30,181 @@ const Registration = () => {
 		setShowPassword(!showPassword);
 	};
 
-	const navigate = useNavigate();
-	const navigateLogin = () => {
-		navigate('/auth/login');
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+		reset
+	} = useForm<ErrorObject>({ mode: 'onBlur' });
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const onSubmit = (data: any) => {
+		if (password !== confirmPassword) {
+			alert('Пароли не совпадают');
+			return null;
+		}
+
+		console.log(data);
+
+		setConfirmPassword('');
+		reset();
 	};
 
 	return (
 		<div className={scss.backHeader}>
 			<div className={scss.Registration}>
 				<div className="container">
-					<div className={scss.bar}>
-						<img src={peakSpaceImg} alt="" />
+					<form onSubmit={handleSubmit(onSubmit)} className={scss.bar}>
+						<img src={peakSpaceImg} alt="Peak Space" />
 						<div className={scss.inputs}>
-							<CustomInput
-								type="text"
-								placeholder="Повторите пароль "
-								onChange={handleInputChange}
-								value={inputValue}
+							<Controller
+								name="Фамилия"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Это поле обязательно к заполнению' }}
+								render={({ field }) => (
+									<Input
+										className={scss.inputPassword}
+										{...field}
+										placeholder="Введите логин"
+										type="text"
+										style={{
+											borderColor: errors.Фамилия ? 'red' : '',
+											backgroundColor: errors.Фамилия
+												? 'rgba(255, 0, 0, 0.122)'
+												: ''
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+									/>
+								)}
 							/>
-							<CustomInput
-								type="text "
-								placeholder="Имя"
-								onChange={handleInputChange}
-								value={inputValue}
+							{errors?.Фамилия && (
+								<p className={scss.textError}>{errors.Фамилия.message}</p>
+							)}
+							<Controller
+								name="Имя"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Это поле обязательно к заполнению' }}
+								render={({ field }) => (
+									<Input
+										className={scss.inputPassword}
+										{...field}
+										placeholder="Имя"
+										type="text"
+										style={{
+											borderColor: errors.Имя ? 'red' : '',
+											backgroundColor: errors.Имя
+												? 'rgba(255, 0, 0, 0.122)'
+												: '',
+											outline: 'none'
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+									/>
+								)}
 							/>
-							<CustomInput
-								type="text"
-								placeholder="Имя пользователя"
-								onChange={handleInputChange}
-								value={inputValue}
+							{errors?.Имя && (
+								<p className={scss.errorName}>{errors.Имя.message}</p>
+							)}
+							<Controller
+								name="nameUser"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Это поле обязательно к заполнению' }}
+								render={({ field }) => (
+									<Input
+										className={scss.inputPassword}
+										{...field}
+										placeholder="Имя пользователя"
+										type="text"
+										style={{
+											borderColor: errors.nameUser ? 'red' : '',
+											backgroundColor: errors.nameUser
+												? 'rgba(255, 0, 0, 0.122)'
+												: '',
+											outline: 'none'
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+									/>
+								)}
 							/>
-							<CustomInput
-								type="emain"
-								placeholder="Номер телефона или email"
-								onChange={handleInputChange}
-								value={inputValue}
+							{errors?.nameUser && (
+								<p className={scss.errorUserName}>{errors.nameUser.message}</p>
+							)}
+							<Controller
+								name="email"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Пожалуйста, введите ваш email.' }}
+								render={({ field }) => (
+									<Input
+										className={scss.inputPassword}
+										{...field}
+										placeholder="Номер телефона или email"
+										type="email"
+										style={{
+											borderColor: errors.email ? 'red' : '',
+											backgroundColor: errors.email
+												? 'rgba(255, 0, 0, 0.122)'
+												: '',
+											outline: 'none'
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+									/>
+								)}
 							/>
-							<Input.Password
-								value={password}
-								onChange={handlePasswordChange}
-								iconRender={(visible) =>
-									visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
-								}
-								placeholder="Пароль"
-								onPressEnter={() => console.log('Нажат Enter')}
-								className={scss.inputPassword}
-								visibilityToggle
-								type={showPassword ? 'text' : 'password'}
+							{errors?.email && (
+								<p className={scss.errorEmail}>{errors.email.message}</p>
+							)}
+							<Controller
+								name="password"
+								control={control}
+								defaultValue=""
+								rules={{ required: 'Пароль обязателен к заполнению' }}
+								render={({ field }) => (
+									<Input.Password
+										{...field}
+										iconRender={(visible) =>
+											visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+										}
+										placeholder=" Пароль"
+										className={scss.inputPassword}
+										visibilityToggle
+										type={showPassword ? 'text' : 'password'}
+										style={{
+											borderColor: errors.password ? 'red' : '',
+											backgroundColor: errors.password
+												? 'rgba(255, 0, 0, 0.122)'
+												: '',
+											outline: 'none'
+										}}
+										onFocus={(e) => {
+											e.target.style.borderColor = '';
+											e.target.style.backgroundColor = '';
+										}}
+										onChange={(e) => {
+											field.onChange(e);
+											setPassword(e.target.value);
+										}}
+									/>
+								)}
 							/>
+							{errors.password && (
+								<p className={scss.errorPassword}>{errors.password.message}</p>
+							)}
+
 							<Input.Password
 								value={confirmPassword}
 								onChange={handleConfirmPasswordChange}
@@ -84,7 +212,6 @@ const Registration = () => {
 									visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
 								}
 								placeholder="Повторите пароль"
-								onPressEnter={() => console.log('Нажат Enter')}
 								className={scss.inputPassword}
 								visibilityToggle
 								type={showPassword ? 'text' : 'password'}
@@ -94,14 +221,12 @@ const Registration = () => {
 								checked={showPassword}
 								onChange={togglePasswordVisibility}
 							>
-								<p className={scss.text}>Сохранть вход</p>
+								<p className={scss.text}>Сохранить вход</p>
 							</Checkbox>
 						</div>
-						<CustomButtonBold
-							children="Зарегистрироваться"
-							onClick={navigateLogin}
-						/>
-					</div>
+						<CustomButtonBold children="Зарегистрироваться" type="submit" />
+						<Link to="/auth/login">Уже есть аккаунт? Войти</Link>
+					</form>
 				</div>
 			</div>
 		</div>
