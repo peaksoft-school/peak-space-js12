8b9ced4a99d806de83a1651152e8c3feb1071c36
Kasym <ulanbek.kasym@gmail.com>
2024-04-16 16:50:44 +0300

    test

--- src/UI/myFriends/MyFriends.tsx
+++ src/UI/myFriends/MyFriends.tsx
@@ -1,7 +1,9 @@
 import { useGetMyFriendsQuery } from '@/src/redux/api/myFriends';
 
 const MyFriends = () => {
-	const { data: MyFriends = [], error } = useGetMyFriendsQuery();
+	const { data, error, refetch } = useGetMyFriendsQuery();
+	console.log(data, error, refetch);
+
 	return <div>MyFriends</div>;
 };
 
