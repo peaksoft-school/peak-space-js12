b92c4067bb639b11568b9a00cdc5fa7f79438d94
Alihan <isaevsali@gmail.com>
2024-07-03 15:36:03 +0600

    fix one

--- src/ui/userInfoLogout/UserInfoLogout.tsx
+++ src/ui/userInfoLogout/UserInfoLogout.tsx
@@ -1,44 +1,39 @@
 import { FC, useState } from 'react';
 import scss from './UserInfoLogout.module.scss';
+import elipse from '../../assets/Rectangle 76.svg';
 import { IconLogout } from '@tabler/icons-react';
 import { Avatar } from 'antd';
 import { useGetMeQuery } from '@/src/redux/api/auth';
+import { useNavigate } from 'react-router-dom';
 
 const UserInfoLogout: FC = () => {
 	const { data } = useGetMeQuery();
 	const [dropdownVisible, setDropdownVisible] = useState(false);
+	const navigate = useNavigate();
 
 	const toggleDropdown = () => {
 		setDropdownVisible(!dropdownVisible);
 	};
 
-	const closeDropdown = () => {
+	const handleLogout = () => {
+		navigate('/auth/login');
 		setDropdownVisible(false);
 	};
 
 	return (
 		<div className={scss.UserInfoLogout} onClick={(e) => e.stopPropagation()}>
 			<div className={scss.user_profile} onClick={toggleDropdown}>
-				<Avatar size={40} icon={<img src={data?.avatar} alt="avatar" />} />
+				<Avatar size={40} icon={<img src={elipse} alt="avatar" />} />
 				<p className={scss.user_name}>{data?.userName}</p>
 			</div>
 			{dropdownVisible && (
 				<div
 					className={`${scss.dropdown} ${dropdownVisible ? scss.dropdownVisible : ''}`}
 					onClick={(e) => e.stopPropagation()}
+					style={{ marginTop: '6rem' }}
 				>
 					<div className={scss.user_data}>
-						<p className={scss.user_email}>{data?.email}</p>
-						<div className={scss.existing_accounts}>
-							<div className={scss.account}>
-								<p>existing_user_1@example.com</p>
-							</div>
-							<div className={scss.account}>
-								<p>existing_user_2@example.com</p>
-							</div>
-						</div>
-						<button className={scss.add_account}>+ Добавить аккаунт</button>
-						<button className={scss.logout} onClick={closeDropdown}>
+						<button className={scss.logout} onClick={handleLogout}>
 							<IconLogout stroke={2} />
 							Выйти
 						</button>
