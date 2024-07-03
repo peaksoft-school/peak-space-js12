90e01b4ff727fe1ddd1a268c3e32fedeb581b52e
Elcho911 <boss.armsport@gmail.com>
2024-07-03 16:18:54 +0600

    remove checkbox

--- src/middleware/UserConnecting.tsx
+++ src/middleware/UserConnecting.tsx
@@ -1,7 +1,9 @@
-import { FC, ReactNode, useEffect, useState } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import scss from './UserConnecting.module.scss';
 import { createPortal } from 'react-dom';
 import { useGetMeQuery } from '../redux/api/auth';
+import { IconPhoneCall, IconPhoneOff } from '@tabler/icons-react';
+import { audio } from '../music';
 
 interface UserConnectingProps {
 	children: ReactNode;
@@ -21,14 +23,37 @@ const UserConnecting: FC<UserConnectingProps> = ({ children }) => {
 	const [opened, setOpened] = useState(false);
 	const [whoCalling, setWhoCalling] = useState<CallRequestPayload>();
 	const [modalContainer] = useState(() => document.createElement('div'));
-	let socket: WebSocket;
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const socketRef = useRef<WebSocket | null>(null);
+	const playButtonRef = useRef<HTMLButtonElement>(null);
 
-	const openModal = () => setOpened(true);
-	const closeModal = () => setOpened(false);
+	const openModal = () => {
+		setOpened(true);
+		setTimeout(() => {
+			// @ts-ignore
+			playButtonRef.current.click();
+		}, 1000);
+	};
+
+	const closeModal = () => {
+		setOpened(false);
+		if (audioRef.current) {
+			audioRef.current.pause();
+			audioRef.current.currentTime = 0;
+		}
+	};
+
+	const handlePlayButton = () => {
+		if (audioRef.current) {
+			audioRef.current.src = audio;
+			audioRef.current
+				.play()
+				.catch((err) => console.error('Error playing audio: ', err));
+		}
+	};
 
 	useEffect(() => {
 		document.body.appendChild(modalContainer);
-
 		return () => {
 			document.body.removeChild(modalContainer);
 		};
@@ -36,28 +61,26 @@ const UserConnecting: FC<UserConnectingProps> = ({ children }) => {
 
 	useEffect(() => {
 		if (!data?.userName || !data?.email) return;
-		// Функция подключения к WebSocket
+
 		const connectWebSocket = () => {
-			socket = new WebSocket(socketUrl);
+			const socket = new WebSocket(socketUrl);
 
 			socket.onopen = () => {
 				console.log('Connected to WebSocket server');
-
-				// Отправка сообщения о подключении пользователя
 				const userDetails = {
 					type: 'connect',
-					name: data?.userName,
-					email: data?.email
+					name: data.userName,
+					email: data.email
 				};
 				socket.send(JSON.stringify(userDetails));
-				console.log(userDetails);
 			};
 
 			socket.onmessage = (event) => {
 				const message = JSON.parse(event.data);
-				console.log('CallMe Received message:', message);
-				setWhoCalling(message.payload);
-				openModal();
+				if (message.payload) {
+					setWhoCalling(message.payload);
+					openModal();
+				}
 			};
 
 			socket.onclose = () => {
@@ -67,40 +90,56 @@ const UserConnecting: FC<UserConnectingProps> = ({ children }) => {
 			socket.onerror = (error) => {
 				console.error('WebSocket error:', error);
 			};
+
+			socketRef.current = socket;
 		};
 
-		// Подключение к WebSocket при монтировании компонента
 		connectWebSocket();
 
-		// Закрытие соединения при размонтировании компонента
 		return () => {
-			if (socket) {
-				socket.close();
+			if (socketRef.current) {
+				socketRef.current.close();
 			}
 		};
-	}, [data, socketUrl]);
+	}, [data]);
 
 	return (
 		<>
 			{children}
+			<audio ref={audioRef} />
 			{opened &&
 				createPortal(
 					<div className={scss.UserConnecting}>
 						<div className={scss.content}>
-							<button onClick={closeModal} className={scss.close_button}>
-								Close
+							<img
+								src={
+									whoCalling?.image
+										? whoCalling.image
+										: 'https://elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Felcho911.eabc74a3.png&w=640&q=75'
+								}
+								alt={whoCalling?.name}
+							/>
+							<div className={scss.user_info}>
+								<h1 className={scss.name}>{whoCalling?.name}</h1>
+								<h1 className={scss.email}>{whoCalling?.email}</h1>
+							</div>
+							<button
+								ref={playButtonRef}
+								onClick={handlePlayButton}
+								style={{ display: 'none' }}
+							>
+								Play Audio
 							</button>
-							<h1>This is a custom modal</h1>
-							<div>
-								<img src={whoCalling?.image} alt={whoCalling?.name} />
-								<h1>{whoCalling?.name}</h1>
-								<h1>{whoCalling?.email}</h1>
+							<div className={scss.buttons}>
 								<button
+									className={scss.call_on}
 									onClick={() => window.open(whoCalling?.callUrl, '_self')}
 								>
-									Принять вызов
+									<IconPhoneCall stroke={2} />
 								</button>
-								<button onClick={closeModal}>Отменить вызов</button>
+								<button className={scss.close_button} onClick={closeModal}>
+									<IconPhoneOff stroke={2} />
+								</button>
 							</div>
 						</div>
 					</div>,
